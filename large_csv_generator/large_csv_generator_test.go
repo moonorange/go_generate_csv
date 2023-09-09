9324d53bb0064d6ff544751ed76d39f7d2d8c09d
moonorange <monoma632@gmail.com>
2023-09-09 12:07:48 +0900

    add: benchmarking

--- large_csv_generator/large_csv_generator_test.go
+++ large_csv_generator/large_csv_generator_test.go
@@ -2,6 +2,7 @@ package large_csv_generator
 
 import (
 	"encoding/csv"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -9,17 +10,24 @@ import (
 )
 
 const (
-	totalNumRows = 10000000
-	fileName     = "test_data"
+	fileName  = "test_data"
+	fileName2 = "test_data2"
 )
 
-func BenchmarkGenerateLargeCSVParallel(b *testing.B) {
-	GenerateLargeCSVParallel(totalNumRows/10, 10, fileName)
-	defer CleanUp()
-
+var testCases = []struct {
+	totalNumRows  int
+	numGoroutines int
+}{
+	{totalNumRows: 100000000, numGoroutines: 5},
+	{totalNumRows: 100000000, numGoroutines: 10},
+	{totalNumRows: 100000000, numGoroutines: 15},
+	{totalNumRows: 100000000, numGoroutines: 20},
 }
 
 func BenchmarkGenerateLargeCSV(b *testing.B) {
+	SetUp()
+	defer CleanUp()
+
 	file, err := os.Create(fmt.Sprintf("data/%s.csv", fileName))
 	if err != nil {
 		panic(err)
@@ -32,13 +40,33 @@ func BenchmarkGenerateLargeCSV(b *testing.B) {
 	}()
 
 	writer := csv.NewWriter(file)
-	GenerateLargeCSV(totalNumRows, writer)
-	defer CleanUp()
+	b.Run(fmt.Sprintf("totalNumRows=%d", 100000000), func(b *testing.B) {
+		GenerateLargeCSV(100000000, writer)
+	})
+	for _, tc := range testCases {
+		b.Run(fmt.Sprintf("totalNumRows=%d,numGoroutines=%d", tc.totalNumRows, tc.numGoroutines), func(b *testing.B) {
+			GenerateLargeCSVParallelToOneFile(tc.totalNumRows/tc.numGoroutines, tc.numGoroutines, fileName2)
+		})
+	}
 }
 
-func BenchmarkGenerateLargeCSVParallelToOneFile(b *testing.B) {
-	GenerateLargeCSVParallelToOneFile(totalNumRows/10, 10, fileName)
-	defer CleanUp()
+// func BenchmarkGenerateLargeCSVParallelToOneFile(b *testing.B) {
+// 	SetUp()
+// 	for _, tc := range testCases {
+// 		b.Run(fmt.Sprintf("totalNumRows=%d,numGoroutines=%d", tc.totalNumRows, tc.numGoroutines), func(b *testing.B) {
+// 			GenerateLargeCSVParallelToOneFile(tc.totalNumRows/tc.numGoroutines, tc.numGoroutines, fileName)
+// 		})
+// 	}
+// 	defer CleanUp()
+// }
+
+func SetUp() {
+	err := os.Mkdir("data", 0777)
+	if err != nil {
+		if !errors.Is(err, os.ErrExist) {
+			log.Fatal(err)
+		}
+	}
 }
 
 func CleanUp() {
